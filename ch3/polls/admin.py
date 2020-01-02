1b054b0a3969ddbf5c517286e867cd067a990afe
심정민 <shjj09@naver.com>
2020-01-02 15:55:09 +0900

    question field modify

--- ch3/polls/admin.py
+++ ch3/polls/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from polls.models import Question, Choice
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+    ('Question Statement',{'fields': ['question_text']}),
+    ('Date information',{'fields': ['pub_date'],'classes':['collapse']}), #collapse는 숨김 
+    ] #필드 순서 변경
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
