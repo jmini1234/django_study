9fdfc8cadab3d4e4fddd7d415122d299172ea718
심정민 <shjj09@naver.com>
2020-01-03 17:54:20 +0900

    comment show

--- ch3/blog/admin.py
+++ ch3/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from blog.models import Post #그냥 model은 error
+from blog.models import Post,Comment #그냥 model은 error
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
